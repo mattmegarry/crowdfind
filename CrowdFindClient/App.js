53993966df5b702cb9936c727db7ed51336d9973
Matt Megarry <mattmegarry@gmail.com>
2020-06-08 18:00:00 +0100

    WIP: Join session request from client

--- CrowdFindClient/App.js
+++ CrowdFindClient/App.js
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import CustomButton from "./Button.js";
 
 export default function App() {
   const [joinSessionSelection, setJoinSessionSelection] = useState(false);
+  const [sessionCode, setSessionCode] = useState("");
+  const [loading, setLoading] = useState(false);
 
   function handleJoinSessionSelect() {
     setJoinSessionSelection(true);
+  }
+
+  function joinSessionRequest() {
+    setLoading(true);
+    fetch("http://localhost:4000/session/join", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        sessionCode: sessionCode
+      })
+    });
   }
 
   return (
@@ -15,22 +31,34 @@ export default function App() {
         <Text style={styles.title}>CrowdFind MVP</Text>
       </View>
       <View style={styles.main}>
-        <Text style={styles.explainerText}>Welcome to CrowdFind!</Text>
-        <Text style={styles.explainerText}>
-          All data is deleted after 3 hours!
-        </Text>
-        {joinSessionSelection ? (
-          <View style={styles.buttonsContainer}>
-            <Text>INPUT FORM GOES HERE.</Text>
-          </View>
+        {loading ? (
+          <Text>LOADING....</Text>
         ) : (
-          <View style={styles.buttonsContainer}>
-            <CustomButton buttonMessage="Create Session"></CustomButton>
-            <CustomButton
-              onPress={handleJoinSessionSelect}
-              buttonMessage="Join Session"
-            ></CustomButton>
-          </View>
+          <>
+            <Text style={styles.explainerText}>Welcome to CrowdFind!</Text>
+            <Text style={styles.explainerText}>
+              All data is deleted after 3 hours!
+            </Text>
+            {joinSessionSelection ? (
+              <View style={styles.buttonsContainer}>
+                <TextInput
+                  style={{ height: 40 }}
+                  placeholder="Enter three word code..."
+                  onChangeText={text => setSessionCode(text)}
+                  defaultValue={sessionCode}
+                  onSubmitEditing={joinSessionRequest}
+                />
+              </View>
+            ) : (
+              <View style={styles.buttonsContainer}>
+                <CustomButton buttonMessage="Create Session"></CustomButton>
+                <CustomButton
+                  onPress={handleJoinSessionSelect}
+                  buttonMessage="Join Session"
+                ></CustomButton>
+              </View>
+            )}
+          </>
         )}
       </View>
     </View>
